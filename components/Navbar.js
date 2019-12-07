c14861b1a86518e751e830b04d6c0026763186da
Garrett Thompson <45501825+thompsg20@users.noreply.github.com>
2019-12-06 18:54:54 -0600

    banner added to navbar
    hope this works lol

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ export default class Navbar extends React.Component {
         return (
             <div className="navContainer">
                 <div className="sidebar">
-                    <div className="item" stye={{ display: "flex"}}>
+                    <div className="item" style={{ display: "flex"}}>
                         <a href="https://hacknewyear.com/" style={{ display: "flex" }}><img src="../static/htny-logo.png" style={{ width: "100%", margin: "auto",maxWidth:"50px" }} /></a>
                     </div>
                     <a href="#faq" className="item">
@@ -15,6 +15,7 @@ export default class Navbar extends React.Component {
                     <a href="#team" className="item">
                         <p>Team</p>
                     </a>
+                    <a href="https://hackclub.com/" target="_blank" style="float:right;"><img src="../static/hackclub-banner.svg"></a>
                 </div>
             </div>
         )
