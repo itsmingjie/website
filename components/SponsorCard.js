b516781da94f8ad15c57e467ff1a08da06d83f57
Neel Redkar <neel.redkar@outlook.com>
2019-12-20 15:47:51 -0800

    fix sponsors

--- components/SponsorCard.js
+++ components/SponsorCard.js
@@ -7,10 +7,19 @@ export default function SponsorCard (props) {
             <style jsx>{`
             img {
                 height:10vh;
+                max-width:20vw;
                 margin:10px;
+                display:block;
+                margin-left: auto;
+                margin-right: auto;
                 border-radius:15px;
+            }
+            a {
+                width:20vw;
+                height:10vh;
+                padding:10px;
             }
             `}</style>
         </a>
     )
-}
+}``
