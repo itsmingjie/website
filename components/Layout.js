5a2b0e9f370a0b688b7045b5a3a0884dab559323
Neel Redkar <neel.redkar@outlook.com>
2019-12-20 17:41:37 -0800

    add gradient:

--- components/Layout.js
+++ components/Layout.js
@@ -25,7 +25,10 @@ export default class Layout extends React.Component {
                     min-height:100%;
                     width:100%;
                     margin:0;
-                    background-color:#2a0188;
+                    background: #ff00cc;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #333399, #ff00cc);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #333399, #ff00cc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
                     position: relative;
                 }
                 body {
