ce18136c7f3c24c095cf3d1c19c47597f404fd34
Neel Redkar <neel.redkar@outlook.com>
2019-12-20 17:45:13 -0800

    add box color

--- components/Box.js
+++ components/Box.js
@@ -1,7 +1,7 @@
 export default class Box extends React.Component {
     render() {
         return(
-            <div style={{padding:"15px",borderRadius:"30px",backgroundColor:"#4b24a4",display:"flex",color:"white",boxShadow:"3px 3px 3px #201539",flexDirection:"column"}}>
+            <div style={{padding:"15px",borderRadius:"30px",backgroundColor:"#8a56ff",display:"flex",color:"white",boxShadow:"3px 3px 3px #201539",flexDirection:"column"}}>
                 {this.props.children}
             </div>
         )
